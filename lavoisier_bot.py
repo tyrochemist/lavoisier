2d659db8cc625b620d21e00182d95caddd86be59
tyrochymicus <tyler.roche@gmail.com>
2020-09-07 23:34:34 -0400

    added embed
    Added embed functionality with formula, molecular weight, and smiles, as well as chemspider URL.

--- lavoisier_bot.py
+++ lavoisier_bot.py
@@ -21,7 +21,17 @@ async def on_message(message):
         # Does search on name, returns first result as compound object
         comp = util.getCompound(cname)
         # Sends image URL as message, which becomes image embed
-        await message.channel.send(comp.image_url)
+        compembed = discord.Embed(
+            title=comp.common_name, color=discord.Colour.purple())
+        compembed.set_image(url=comp.image_url)
+        compembed.add_field(
+            name="Formula", value=f"`{comp.molecular_formula}`")
+        compembed.add_field(name="SMILES", value=comp.smiles)
+        compembed.add_field(name="Molecular Weight",
+                            value=comp.molecular_weight)
+        compembed.add_field(
+            name="URL", value=f"https://www.chemspider.com/Chemical-Structure.{comp.record_id}.html")
+        await message.channel.send(embed=compembed)
 
 
 client.run("CLIENT_TOKEN")
